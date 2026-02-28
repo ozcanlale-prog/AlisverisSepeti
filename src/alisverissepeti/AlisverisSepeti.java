646b120cb09a14dc451f3800d7811fefe047dff5
25020091010ÖzcanLale <25020091010@ogr.istiklal.edu.tr>
2026-02-28 16:19:47 +0300

    Add files via upload

---
+++ src/alisverissepeti/AlisverisSepeti.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package alisverissepeti;
+
+/**
+ *
+ * @author özcan
+ */
+public class AlisverisSepeti {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
